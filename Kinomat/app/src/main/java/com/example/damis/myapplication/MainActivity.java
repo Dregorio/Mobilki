2c53aa302731f2564cfc1c576e3ba18449f6d80b
Dregorio <luks9_9@o2.pl>
2017-12-05 14:30:37 +0100

    project

--- Kinomat/app/src/main/java/com/example/damis/myapplication/MainActivity.java
+++ Kinomat/app/src/main/java/com/example/damis/myapplication/MainActivity.java
@@ -1,13 +1,37 @@
 package com.example.damis.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = (Button)findViewById(R.id.button2);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        Button regster = (Button)findViewById(R.id.btn_register);
+
+        regster.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
